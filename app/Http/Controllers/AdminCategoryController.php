3df0707d7d359fda4e8d1dba92d72a37f5c2fbb8
Mak Moinee <titus.silver.ph@gmail.com>
2024-05-28 04:06:45 +0800

    update code

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -67,6 +67,7 @@ class AdminCategoryController extends Controller
             }
 
             if ($request->btnCreateCategory) {
+                dd($request);
                 $query = DB::table('categories')->where('categoryName', '=', $request->categoryName)->get();
                 $data = json_decode($query, true);
                 if (count($data) > 0) {
